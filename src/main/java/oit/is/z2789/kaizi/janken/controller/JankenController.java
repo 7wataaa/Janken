cb2b0f8a33b791124f74911f9cd001856fdca698
7wataaa <65275342+7wataaa@users.noreply.github.com>
2025-10-13 00:46:39 +0900

    ユーザ名を入力し，janken.htmlを表示する処理を実装

--- src/main/java/oit/is/z2789/kaizi/janken/controller/JankenController.java
+++ src/main/java/oit/is/z2789/kaizi/janken/controller/JankenController.java
@@ -1,11 +1,19 @@
+package oit.is.z2789.kaizi.janken.controller;
+
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
+@RequestMapping("/janken")
 public class JankenController {
 
-  @GetMapping
-  String janken() {
+  @PostMapping
+  String janken(@RequestParam String user_id, ModelMap model) {
+    model.addAttribute("user_id", user_id);
+
     return "janken.html";
   }
 }
--- src/main/java/oit/is/z2789/kaizi/janken/model/Janken.java
+++ src/main/java/oit/is/z2789/kaizi/janken/model/Janken.java
@@ -1,3 +1,5 @@
+package oit.is.z2789.kaizi.janken.model;
+
 public class Janken {
-  
+
 }
